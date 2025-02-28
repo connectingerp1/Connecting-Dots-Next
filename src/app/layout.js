2388d3470d4d41e3701ed3cf86e2cfe0b5ff714b
Yash <yash.hulle3699@gmail.com>
2025-02-28 12:19:17 +0530

    Added Google Tag Manager for testing

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,27 +12,51 @@ import Chatbot from "@/components/Chatbot";
 import Whatsapp from "@/components/Whatsapp";
 import Floatingcontact from "@/components/Floatingcontact";
 import BottomMenu from "@/components/BottomMenu";
+import Script from "next/script"; // Import Next.js Script
 
 export const metadata = {
   title: "Software Training in Pune with placement support",
   description: "Connecting Dots ERP",
 };
 
+// Your Google Tag Manager & Search Console verification
+const GTM_ID = "GTM-MB68QM2V"; // Replace with your GTM ID
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Tag Manager - HEAD */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l; j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${GTM_ID}');
+            `,
+          }}
+        />
+
+      </head>
       <body className="body" cz-shortcut-listen="true">
-        <div className="background-animation">
-          <div className="starsec"></div>
-          <div className="starthird"></div>
-          <div className="starfourth"></div>
-          <div className="starfifth"></div>
-        </div>
+        {/* Google Tag Manager - BODY (noscript fallback) */}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `
+              <iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>
+            `,
+          }}
+        />
+
         <CallAdvisorsStrip />
         <Marquee />
         <Navbar />
         <CityProvider>{children}</CityProvider>
-
         <Floatingcontact phoneNumber="+919004002958" />
         <Whatsapp phoneNumber="+919004002958" />
         <Chatbot />
